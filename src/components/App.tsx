d2eb5f6da32627d04fa560b41fb797d521cea3a8
r-miyamae <ryohei_miyamae@voyagegroup.com>
2020-05-27 20:07:34 +0900

    wip add label

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Item, Todo } from "./Item";
+import { Item, Todo, Label, LabelItem } from "./Item";
 
 function getUniqueStr() {
   var strong = 1000;
@@ -14,9 +14,16 @@ const App: React.FunctionComponent = () => {
     id: "0",
     title: "",
     description: "",
+    labels: [],
   });
   const [todos, setTodos] = React.useState<Todo[]>([]);
   const [done_todo_ids, setDoneTodoIds] = React.useState<string[]>([]);
+
+  const [label, setLabel] = React.useState<Label>({
+    id: "0",
+    name: "",
+  });
+  const [labels, setLabels] = React.useState<Label[]>([]);
 
   React.useEffect(() => {
     console.log("mount");
@@ -30,17 +37,26 @@ const App: React.FunctionComponent = () => {
     console.log(todo);
     console.log(todos);
     console.log(done_todo_ids);
-  }, [todo, todos, done_todo_ids]);
+    console.log(label);
+  }, [todo, todos, done_todo_ids, label, labels]);
 
-  const onClick = () => {
+  const onClick = (resource: "todo" | "label") => () => {
     console.log("add");
-    console.log(todo);
-    const target = { ...todo };
-    target["id"] = getUniqueStr();
-    setTodo(target);
-    const targets = [...todos, target];
-    console.log(targets);
-    setTodos(targets);
+    if (resource === "todo") {
+      console.log(todo);
+      const target = { ...todo };
+      target["id"] = getUniqueStr();
+      // setTodo(target);
+      const targets = [...todos, target];
+      console.log(targets);
+      setTodos(targets);
+    } else {
+      const target = { ...label };
+      target["id"] = getUniqueStr();
+      const targets = [...labels, target];
+      console.log(labels);
+      setLabels(targets);
+    }
   };
 
   const onChange = (key: "title" | "description") => (
@@ -53,6 +69,13 @@ const App: React.FunctionComponent = () => {
       setTodo(target);
       console.log(todo);
     }
+  };
+
+  const onLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("labelchange");
+    const target = { ...label };
+    target["name"] = event.target.value;
+    setLabel(target);
   };
 
   const toggleDone = (id: string) => () => {
@@ -80,34 +103,54 @@ const App: React.FunctionComponent = () => {
   return (
     <React.Fragment>
       <h1>Super TODO Practice</h1>
-      <p>
-        <label>
-          タイトル
-          <input type="text" onChange={onChange("title")} />
-        </label>
-      </p>
-      <p>
-        <label>
-          内容
-          <input type="text" onChange={onChange("description")} />
-        </label>
-      </p>
-      <button onClick={onClick}>追加</button>
-      <ul>
-        {todos.map((todo, i) => (
-          <li key={i}>
-            {done_todo_ids.includes(todo.id) ? (
-              <s>
+      <div>
+        <p>
+          <label>
+            タイトル
+            <input type="text" onChange={onChange("title")} />
+          </label>
+        </p>
+        <p>
+          <label>
+            内容
+            <input type="text" onChange={onChange("description")} />
+          </label>
+        </p>
+        <button onClick={onClick("todo")}>追加</button>
+        <ul>
+          {todos.map((todo, i) => (
+            <li key={i}>
+              {done_todo_ids.includes(todo.id) ? (
+                <s>
+                  <Item todo={todo} />
+                </s>
+              ) : (
                 <Item todo={todo} />
-              </s>
-            ) : (
-              <Item todo={todo} />
-            )}
-            <button onClick={toggleDone(todo.id)}>完了</button>
-            <button onClick={onDelete(todo.id)}>削除</button>
-          </li>
-        ))}
-      </ul>
+              )}
+              <button onClick={toggleDone(todo.id)}>完了</button>
+              <button onClick={onDelete(todo.id)}>削除</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <label>
+          ラベル
+          <input
+            type="text"
+            placeholder="ラベル名を入れる"
+            onChange={onLabelChange}
+          />
+        </label>
+        <button onClick={onClick("label")}>追加</button>
+        <ul>
+          {labels.map((label, i) => (
+            <li key={i}>
+              <LabelItem label={label} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -4,16 +4,34 @@ export type Todo = {
   id: string;
   title: string;
   description: string;
+  labels: Label[];
+};
+
+export type Label = {
+  id: string;
+  name: string;
 };
 
 type Props = {
   todo: Todo;
 };
 
+type LabelProps = {
+  label: Label;
+};
+
 export const Item: React.FunctionComponent<Props> = ({ todo }) => (
   <div>
     <p>
       {todo.id}: {todo.title}: {todo.description}
+    </p>
+  </div>
+);
+
+export const LabelItem: React.FunctionComponent<LabelProps> = ({ label }) => (
+  <div>
+    <p>
+      {label.id}: {label.name}
     </p>
   </div>
 );
